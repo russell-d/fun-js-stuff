1ef2d113cabb779cff0638562091677909b5e0f2
russell-d <rdevera@csumb.edu>
2022-10-18 16:02:34 -0700

    init add

---
+++ Week 9 - Rasterization Pipeline2/misc/camera.js
@@ -0,0 +1,46 @@
+function Camera(input) {
+    // The following two parameters will be used to automatically create the cameraWorldMatrix in this.update()
+    this.cameraYaw = 0;
+    this.cameraPosition = new Vector3();
+
+    this.cameraWorldMatrix = new Matrix4();
+
+    // -------------------------------------------------------------------------
+    this.getViewMatrix = function() {
+        return this.cameraWorldMatrix.clone().inverse();
+    }
+
+    // -------------------------------------------------------------------------
+    this.getForward = function() {
+        // todo #6 - pull out the forward direction from the world matrix and return as a vector
+        //         - recall that the camera looks in the "backwards" direction
+        return new Vector3();
+    }
+    // -------------------------------------------------------------------------
+    this.update = function(dt) {
+        var currentForward = this.getForward();
+
+        if (input.up) {
+            // todo #7 - move the camera position a little bit in its forward direction
+        }
+
+        if (input.down) {
+            // todo #7 - move the camera position a little bit in its backward direction
+        }
+
+        if (input.left) {
+            // todo #8 - add a little bit to the current camera yaw
+        }
+
+        if (input.right) {
+            // todo #8 - subtract a little bit from the current camera yaw
+        }
+
+        // todo #7 - create the cameraWorldMatrix from scratch based on this.cameraPosition
+
+        // todo #8 - create a rotation matrix based on cameraYaw and apply it to the cameraWorldMatrix
+        // (order matters!)
+    }
+}
+
+// EOF 00100001-10
