62bb5284820e0caf082e08bfc9b5bbc631e7d57d
russell-d <rdevera@csumb.edu>
2022-11-28 17:07:54 -0800

    init add

---
+++ Week 12 - Shadow Mapping/shaders/depth-write.vs.glsl
@@ -0,0 +1,13 @@
+
+attribute vec3 aVertexPosition;
+uniform mat4 uWorldMatrix;
+uniform mat4 uViewMatrix;
+uniform mat4 uProjectionMatrix;
+varying float vDepth;
+
+void main(void) {
+  gl_Position = uProjectionMatrix * uViewMatrix * uWorldMatrix * vec4(aVertexPosition, 1.0);
+  // todo #3 convert clip space depth into NDC and rescale from [-1, 1] to [0, 1]
+  vDepth = gl_Position.z;  // temporarily set to gl_Position.z
+}
+// EOF 00100001-10
